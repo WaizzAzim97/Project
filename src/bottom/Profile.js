0ef06e41847e767187665b6aeedb47f5b2657339
spiderdev <akafif16@gmail.com>
2023-04-06 11:37:38 +0800

    another push

--- src/bottom/Profile.js
+++ src/bottom/Profile.js
@@ -1,113 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = () => {
-    let name = '';
-    const navigation = useNavigation();
+  let name = '';
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        getUseData();
-    }, []);
+  useEffect(() => {
+    getUseData();
+  }, []);
 
-    const getUseData = async () => {
-        name = await AsyncStorage.getItem('NAME');
-    };
+  const getUseData = async () => {
+    name = await AsyncStorage.getItem('NAME');
+  };
 
-    return (
-        <View style={styles.container}>
-            <View
-                style={{
-                    width: '100%',
-                    height: 70,
-                    justifyContent: 'space-between',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                }}>
-                <Text
-                    style={{
-                        fontWeight: 'bold',
-                        fontSize: 18,
-                        marginLeft: 15,
-                    }}>
-                    Profile
-                </Text>
-                <TouchableOpacity
-                    style={{
-                        width: 30,
-                        height: 30,
-                        marginRight: 20,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                    {/* <Image
-                        style={{
-                            width: 24,
-                            height: 24,
-                        }}
-                        source={require('../images/settings.png')}
-                    /> */}
-                </TouchableOpacity>
-            </View>
-            <Image
-                source={require('../images/profile.png')}
-                style={{ width: 80, height: 80, alignSelf: 'center', marginTop: 30 }}
-            />
-            <Text style={{ alignSelf: 'center', marginTop: 20, fontSize: 18 }}>
-                Ali
-            </Text>
-            <TouchableOpacity
-                style={{
-                    width: '90%',
-                    alignSelf: 'center',
-                    height: 50,
-                    borderBottomWidth: 0.3,
-                    marginTop: 20,
-                    borderBottomColor: '#8e8e8e',
-                    justifyContent: 'center',
-                }}
-                onPress={() => {
-                    navigation.navigate('Login');
-                }}>
-                <Text>My Address</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={{
-                    width: '90%',
-                    alignSelf: 'center',
-                    height: 50,
-                    borderBottomWidth: 0.3,
-                    marginTop: 20,
-                    borderBottomColor: '#8e8e8e',
-                    justifyContent: 'center',
-                }}
-                onPress={() => {
-                    navigation.navigate('Login');
-                }}>
-                <Text>My Packages</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={{
-                    width: '90%',
-                    alignSelf: 'center',
-                    height: 50,
-                    borderBottomWidth: 0.3,
-                    marginTop: 20,
-                    borderBottomColor: '#8e8e8e',
-                    justifyContent: 'center',
-                }}
-                onPress={() => {
-                    navigation.navigate('Login');
-                }}>
-                <Text>My Saved</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          width: '100%',
+          height: 70,
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: 18,
+            marginLeft: 15,
+          }}>
+          Profile
+        </Text>
+        <TouchableOpacity
+          style={{
+            width: 30,
+            height: 30,
+            marginRight: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image
+            style={{
+              width: 24,
+              height: 24,
+            }}
+            source={require('../images/settings.png')}
+          />
+        </TouchableOpacity>
+      </View>
+      <Image
+        source={require('../images/profile.png')}
+        style={{width: 80, height: 80, alignSelf: 'center', marginTop: 30}}
+      />
+      <Text style={{alignSelf: 'center', marginTop: 20, fontSize: 18}}>
+        Ali
+      </Text>
+      <TouchableOpacity
+        style={{
+          width: '90%',
+          alignSelf: 'center',
+          height: 50,
+          borderBottomWidth: 0.3,
+          marginTop: 20,
+          borderBottomColor: '#8e8e8e',
+          justifyContent: 'center',
+        }}
+        onPress={() => {
+          navigation.navigate('Login');
+        }}>
+        <Text>My Address</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          width: '90%',
+          alignSelf: 'center',
+          height: 50,
+          borderBottomWidth: 0.3,
+          marginTop: 20,
+          borderBottomColor: '#8e8e8e',
+          justifyContent: 'center',
+        }}
+        onPress={() => {
+          navigation.navigate('Login');
+        }}>
+        <Text>My Packages</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          width: '90%',
+          alignSelf: 'center',
+          height: 50,
+          borderBottomWidth: 0.3,
+          marginTop: 20,
+          borderBottomColor: '#8e8e8e',
+          justifyContent: 'center',
+        }}
+        onPress={() => {
+          navigation.navigate('Login');
+        }}>
+        <Text>My Saved</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 export default Profile;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 });
